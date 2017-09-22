6172fa8dfc8800d51122d99f4a92b2ab93ef7a1d
ImAGenji <32185445+ImAGenji@users.noreply.github.com>
2017-09-22 15:46:23 -0700

    Update snake.js

--- snake.js
+++ snake.js
@@ -42,8 +42,8 @@ function draw() {
   if(gameOver === false){
     /*hack ++;
     if(hack > 1000){
-      snake.x.push(snake.x[snake.x.length-100]);
-      snake.y.push(snake.y[snake.y.length-100]);
+      snake.x.push(snake.x[snake.x.length-1]);
+      snake.y.push(snake.y[snake.y.length-1]);
       snake.xV.push(0);
       snake.yV.push(0);
     }*/
@@ -65,7 +65,7 @@ function draw() {
           }
         }
         if(snake.x[i] === apple.x && snake.y[i] === apple.y){
-          for(var t = 0;t < 3;t ++){
+          for(var t = 0;t < 100;t ++){
             snake.x.push(snake.x[snake.x.length-1]);
             snake.y.push(snake.y[snake.y.length-1]);
             snake.xV.push(0);
